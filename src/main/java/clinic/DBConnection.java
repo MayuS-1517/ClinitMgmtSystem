e8ee9ef7e7b5a792a393a738687420bca7e559cb
MayuS-1517 <mayurisurve1517@gmail.com>
2025-07-11 16:44:54 +0530

    Update DBConnection.java

--- src/main/java/clinic/DBConnection.java
+++ src/main/java/clinic/DBConnection.java
@@ -4,9 +4,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:postgresql://localhost:5432/gs_billing";
+    private static final String URL = "jdbc:postgresql://localhost:5432/Clinic_Mgmt";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "bappa@19";
+    private static final String PASSWORD = "123";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
